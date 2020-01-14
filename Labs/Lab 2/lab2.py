5ce425530ef094bce6def088872236f9cec32944
Adrian Darian <adarian@ucmerced.edu>
2020-01-13 22:03:44 -0800

    Downloaded course work

---
+++ Labs/Lab 2/lab2.py
@@ -0,0 +1,15 @@
+def cartesian_product(X, Y):
+# provide your code here
+            
+
+X = [1, 2]
+Y = ['a', 'b', 'c', 'd']
+
+print cartesian_product(X, Y)
+
+
+def power_set(X):
+# provide your code here
+        
+        
+print power_set(Y)
